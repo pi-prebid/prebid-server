5f4fd8afbf99b6302a6fa62779090a9ebf62daea
4lexvav <4lexvav@gmail.com>
2022-10-20 16:37:20 +0300

    use tabs

--- modules/builder.go
+++ modules/builder.go
@@ -1,15 +1,15 @@
 package modules
 
 import (
-    acmeFoobar "github.com/prebid/prebid-server/modules/acme/foobar"
+	acmeFoobar "github.com/prebid/prebid-server/modules/acme/foobar"
 )
 
 // builders returns mapping between module name and its builder
 // vendor and module names are chosen based on the module directory name
 func builders() ModuleBuilders {
-    return ModuleBuilders{
-        "acme": {
-            "foobar": acmeFoobar.Builder,
-        },
-    }
+	return ModuleBuilders{
+		"acme": {
+			"foobar": acmeFoobar.Builder,
+		},
+	}
 }
